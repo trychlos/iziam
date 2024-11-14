c2e144822e6e19e95d502da19f6644438ed92923
Pierre Wieser <p.wieser@trychlos.org>
2024-11-14 09:54:55 +0100

    Import collection2 before attaching ClientsEntities schema

--- imports/common/collections/clients_entities/schema.js
+++ imports/common/collections/clients_entities/schema.js
@@ -8,12 +8,14 @@ import { strict as assert } from 'node:assert';
 import SimpleSchema from 'meteor/aldeed:simple-schema';
 import { Tracker } from 'meteor/tracker';
 
+import 'meteor/aldeed:collection2/static';
+
 import { ClientsEntities } from './index.js';
 
 Tracker.autorun(() => {
     const fieldSet = ClientsEntities.fieldSet?.get();
     if( fieldSet ){
-        console.warn( 'ClientsEntities attaching schema' );
+        console.debug( 'ClientsEntities attaching schema' );
         ClientsEntities.collection.attachSchema( new SimpleSchema( fieldSet.toSchema()), { replace: true });
         console.debug( 'ClientsEntities schema attached' );
         ClientsEntities.collection.attachBehaviour( 'timestampable', { replace: true });
